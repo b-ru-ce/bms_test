35bbdc157bbdf7bda6c57dbefb7fde547af3916d
Chucha <mrswlasowa@gmail.com>
2014-01-09 14:49:30 +0600

    News generator - take away code news of rails_admin

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -28,45 +28,45 @@ class InstallGenerator < Rails::Generators::Base
 
     generate 'cancan:ability'
 
-    gem_group :development do
-      #gem 'annotate', git: 'git://github.com/jeremyolliver/annotate_models.git', branch: 'rake_compatibility'
-      gem 'annotate'
-      gem 'populator'
-      gem 'faker'
-    end
-    run('bundle install')
-
-    generate 'kaminari:views', 'default'
-    generate 'devise:views'
-
-    run('for file in app/views/devise/**/*.erb; do html2haml -e $file ${file%erb}haml && rm $file; done')
-
-    generate 'model', 'MyConfig key:string key_ru:string value:text'
-    generate 'model', 'Role name:string'
-    generate 'migration', 'AddRoleIdToUsers role_id:integer'
-    rake 'db:migrate'
-
-    copy_file 'app/models/ability.rb', 'app/models/ability.rb'
-    copy_file 'app/models/my_config.rb', 'app/models/my_config.rb'
-    copy_file 'app/models/role.rb', 'app/models/role.rb'
-    copy_file 'app/models/user.rb', 'app/models/user.rb'
-
-    copy_file 'tasks/base_fill.rake', 'lib/tasks/base_fill.rake'
-    rake 'db:fill_my_config'
-    rake 'db:fill_users'
+    #gem_group :development do
+    #  #gem 'annotate', git: 'git://github.com/jeremyolliver/annotate_models.git', branch: 'rake_compatibility'
+    #  gem 'annotate'
+    #  gem 'populator'
+    #  gem 'faker'
+    #end
+    #run('bundle install')
+    #
+    #generate 'kaminari:views', 'default'
+    #generate 'devise:views'
+    #
+    #run('for file in app/views/devise/**/*.erb; do html2haml -e $file ${file%erb}haml && rm $file; done')
+    #
+    #generate 'model', 'MyConfig key:string key_ru:string value:text'
+    #generate 'model', 'Role name:string'
+    #generate 'migration', 'AddRoleIdToUsers role_id:integer'
+    #rake 'db:migrate'
+    #
+    #copy_file 'app/models/ability.rb', 'app/models/ability.rb'
+    #copy_file 'app/models/my_config.rb', 'app/models/my_config.rb'
+    #copy_file 'app/models/role.rb', 'app/models/role.rb'
+    #copy_file 'app/models/user.rb', 'app/models/user.rb'
+    #
+    #copy_file 'tasks/base_fill.rake', 'lib/tasks/base_fill.rake'
+    #rake 'db:fill_my_config'
+    #rake 'db:fill_users'
 
     copy_file 'config/initializers/rails_admin.rb', 'config/initializers/rails_admin.rb'
-    copy_file 'config/locales/ru.yml', 'config/locales/ru.yml'
-    copy_file 'config/locales/devise.ru.yml', 'config/locales/devise.ru.yml'
-    copy_file 'config/locales/rails_admin.ru.yml', 'config/locales/rails_admin.ru.yml'
-    copy_file 'config/locales/kaminary.yml', 'config/locales/kaminary.yml'
-    copy_file 'app/controllers/application_controller.rb', 'app/controllers/application_controller.rb'
-    copy_file 'app/helpers/application_helper.rb', 'app/helpers/application_helper.rb'
-
-    application do
-      "config.time_zone = 'Ekaterinburg'"
-      "config.i18n.default_locale = :ru"
-    end
+    #copy_file 'config/locales/ru.yml', 'config/locales/ru.yml'
+    #copy_file 'config/locales/devise.ru.yml', 'config/locales/devise.ru.yml'
+    #copy_file 'config/locales/rails_admin.ru.yml', 'config/locales/rails_admin.ru.yml'
+    #copy_file 'config/locales/kaminary.yml', 'config/locales/kaminary.yml'
+    #copy_file 'app/controllers/application_controller.rb', 'app/controllers/application_controller.rb'
+    #copy_file 'app/helpers/application_helper.rb', 'app/helpers/application_helper.rb'
+    #
+    #application do
+    #  "config.time_zone = 'Ekaterinburg'"
+    #  "config.i18n.default_locale = :ru"
+    #end
 
     #git add: '.'
     #git commit: '-m Base prepare completed'
@@ -75,66 +75,23 @@ class InstallGenerator < Rails::Generators::Base
 
   def news
     if yes?('Would you like to install module News? (y/n)')
-      generate 'controller', 'Articles'
-      copy_file 'app/controllers/articles_controller.rb', 'app/controllers/articles_controller.rb'
+      #generate 'controller', 'Articles'
+      #copy_file 'app/controllers/articles_controller.rb', 'app/controllers/articles_controller.rb'
+      #
+      #generate 'model', 'Article title:text date:date image_uid:string short_text:text text:text title_of_window:string'
+      #rake 'db:migrate'
+      #copy_file 'app/models/article.rb', 'app/models/article.rb'
+      #
+      #copy_file 'app/views/articles/_article.html.haml', 'app/views/articles/_article.html.haml'
+      #copy_file 'app/views/articles/index.html.haml', 'app/views/articles/index.html.haml'
+      #copy_file 'app/views/articles/show.html.haml', 'app/views/articles/show.html.haml'
+      #route "get 'news' => 'articles#index'"
+      #route "get 'articles/:id-:alias' => 'articles#show'"
+      #
+      #copy_file 'tasks/fill_news.rake', 'lib/tasks/fill_news.rake'
+      #rake 'db:fill_news'
 
-      generate 'model', 'Article title:text date:date image_uid:string short_text:text text:text title_of_window:string'
-      rake 'db:migrate'
-      copy_file 'app/models/article.rb', 'app/models/article.rb'
-
-      copy_file 'app/views/articles/_article.html.haml', 'app/views/articles/_article.html.haml'
-      copy_file 'app/views/articles/index.html.haml', 'app/views/articles/index.html.haml'
-      copy_file 'app/views/articles/show.html.haml', 'app/views/articles/show.html.haml'
-      route "get 'news' => 'articles#index'"
-      route "get 'articles/:id-:alias' => 'articles#show'"
-
-      copy_file 'tasks/fill_news.rake', 'lib/tasks/fill_news.rake'
-      rake 'db:fill_news'
-
-      #попробовать потом вынести код в отдельный файл
-      inject_into_file 'config/initializers/rails_admin.rb', after: "RailsAdmin.config do |config|\n" do
-        <<-'RUBY'
-        config.model Article do
-          navigation_label 'Контент'
-          weight -1
-
-          list do
-            field :date
-            field :title
-            field :short_text
-          end
-
-          edit do
-            group :main do
-              label "Главная инфомация"
-              field :title
-              field :date
-              field :short_text
-              field :text, :ck_editor
-              field :image
-            end
-
-            group :seo do
-              label "SEO-оптимизация"
-              field :title_of_window
-            end
-
-          end
-
-          show do
-            field :id
-            field :date
-            field :title
-            field :image
-            field :short_text
-            field :text
-            field :title_of_window
-            field :created_at
-            field :updated_at
-          end
-        end
-      RUBY
-      end
+      insert_into_file 'config/initializers/rails_admin.rb', File.read('config/initializers/rails_admin_news.rb'), after: "RailsAdmin.config do |config|\n"
     end
   end
 
---
+++ lib/generators/install/templates/config/initializers/_rails_admin_news.rb
@@ -0,0 +1,39 @@
+config.model Article do
+  navigation_label 'Контент'
+  weight -1
+
+  list do
+    field :date
+    field :title
+    field :short_text
+  end
+
+  edit do
+    group :main do
+      label "Главная инфомация"
+      field :title
+      field :date
+      field :short_text
+      field :text, :ck_editor
+      field :image
+    end
+
+    group :seo do
+      label "SEO-оптимизация"
+      field :title_of_window
+    end
+
+  end
+
+  show do
+    field :id
+    field :date
+    field :title
+    field :image
+    field :short_text
+    field :text
+    field :title_of_window
+    field :created_at
+    field :updated_at
+  end
+end
