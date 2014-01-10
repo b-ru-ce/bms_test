db256784d04918cce9fe47c49fda0e7bbc3bf158
Chucha <mrswlasowa@gmail.com>
2014-01-10 16:07:15 +0600

    Add method self.next_migration_number

--- lib/generators/install/install_generator.rb
+++ lib/generators/install/install_generator.rb
@@ -2,6 +2,16 @@ class InstallGenerator < Rails::Generators::Base
   include Rails::Generators::Migration
   source_root File.expand_path('../templates', __FILE__)
 
+  def self.next_migration_number(path)
+    unless @prev_migration_nr
+      @prev_migration_nr = Time.now.utc.strftime("%Y%m%d%H%M%S").to_i
+    else
+      @prev_migration_nr += 1
+    end
+    @prev_migration_nr.to_s
+  end
+
+
   def base_prepare
     generate 'model', 'Page title:string text:text purpose:string show_in_menu:boolean menu:string meta:text title_of_window:string ancestry:string sort:integer'
     migration_template 'db/migrate/add_ancestry_to_page', 'db/migrate/add_ancestry_to_page'
